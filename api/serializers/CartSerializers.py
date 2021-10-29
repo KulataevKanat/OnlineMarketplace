03ef0da31c14a22f1cd7c7f6db5fbb5174fa2b68
KulataevKanat <RaketaEnot4466@gmail.com>
2021-10-29 17:27:39 +0600

    Cart serializers

---
+++ api/serializers/CartSerializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+
+from api.models import Cart
+from api.serializers import ProductSerializers
+
+
+class CreateCartSerializer(serializers.ModelSerializer):
+    """Добавление товара в корзину"""
+
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+
+class UpdateCartSerializer(serializers.ModelSerializer):
+    """Обновление товара из корзины по идентификации"""
+
+    class Meta:
+        model = Cart
+        fields = '__all__'
+
+
+class GetCartSerializer(serializers.ModelSerializer):
+    """Вывод товаров из корзины"""
+
+    product_id = ProductSerializers.GetProductSerializer()
+
+    class Meta:
+        model = Cart
+        fields = [
+            'id',
+            'product_id',
+            'amount',
+        ]
