6da6ef6e198e0027c4817b8813aac0e771684d0b
KulataevKanat <RaketaEnot4466@gmail.com>
2021-10-29 19:57:55 +0600

    project exceptions and model statuses

---
+++ OnlineMarketplace/advice.py
@@ -0,0 +1,15 @@
+# exceptions
+USER_DETAIL = "user"
+ACCESS_TOKEN_DETAIL = "access_token"
+USER_NOT_FOUND = "Пользователь не найден."
+USER_BLOCKED = "Пользвователь заблокирован."
+USER_NOT_ACTIVE = "Пользвователь не активен."
+WRONG_PASSWORD = "Неверное имя пользователя или пароль."
+AUTH_CREDENTIALS = "Учетные данные не были предоставлены."
+REFRESH_TOKEN_EXPIRED = "Срок действия токена-обновления истек, пожалуйста, войдите снова."
+ACCESS_TOKEN_EXPIRED = "Срок действия токена-доступа истек."
+TOKEN_PREFIX = "Префикс токена отсутствует."
+
+# statuses
+STATUS_NONE = 'status-none'
+PURCHASED = 'purchased'
